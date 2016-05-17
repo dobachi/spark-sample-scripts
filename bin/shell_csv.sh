f0b529dbcc0467e2b26890de89e1dab735f4c16c
dobachi <dobachi1983oss@gmail.com>
2016-05-18 00:38:56 +0900

    Add downloading data before starting the shell

--- bin/shell_csv.sh
+++ bin/shell_csv.sh
@@ -1,1 +1,8 @@
+DATA_DIR=${DATA_DIR:-/tmp}
+IRIS="iris.data"
+if [ ! -f ${DATA_DIR}/${IRIS} ]; then
+  echo "...Download ${IRIS} to ${DATA_DIR}/${IRIS}"
+  wget http://archive.ics.uci.edu/ml/machine-learning-databases/iris/${IRIS} -O ${DATA_DIR}/${IRIS}
+fi
+
 spark-shell --packages com.databricks:spark-csv_2.10:1.4.0
